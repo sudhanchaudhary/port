71c2d20fd73bb083adb68f9dda2b9761ea1bd7cb
Madhusudhan Chaudhary <sawuriya007@gmail.com>
2026-05-27 17:16:44 +0545

    msg model and form

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    name=models.CharField(max_length=200, null=True, blank=True)
+    email=models.EmailField(null=True,blank=True)
+    message=models.TextField(null=True,blank=True)
+    date=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from .models import Message
+from django.contrib import messages
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        msg=request.POST.get('msg')        
+        Message.objects.create(name=name,email=email,message=msg)
+        messages.success(request,"Message Sent Successfully.")
+            
     return render(request,'home.html')
+
