55ea4fbec066cee6375eeebbf02991b4bd2b1ec8
Madhusudhan Chaudhary <sawuriya007@gmail.com>
2026-06-02 22:22:27 +0545

    Data

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import Message,Skill,Portfolio,TechCategory
+from .models import Message,Skill,Portfolio,TechCategory,Project,Project_Features,Project_Skill
 
 # Register your models here.
 admin.site.register(Message)
 admin.site.register(Skill)
 admin.site.register(Portfolio)
 admin.site.register(TechCategory)
+
+class ProjectFeatureAdmin(admin.TabularInline):
+    model=Project_Features
+    extra=2
+class ProjectSkillAdmin(admin.TabularInline):
+    model=Project_Skill
+    extra=2
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display=['title','desc']
+    inlines=[ProjectFeatureAdmin,ProjectSkillAdmin]
--- app/models.py
+++ app/models.py
@@ -31,3 +31,23 @@ class Skill(models.Model):
     level=models.CharField(choices=Skill_Level)
     def __str__(self):
         return self.skill
+    
+class Project(models.Model):
+    title=models.CharField(max_length=200)
+    desc=models.TextField()
+    github=models.CharField(max_length=300,blank=True,null=True)
+    live=models.CharField(max_length=300,blank=True,null=True)
+    def __str__(self):
+        return self.title
+    
+class Project_Features(models.Model):
+    project=models.ForeignKey(Project,on_delete=models.CASCADE,related_name='feature')
+    feature=models.CharField(max_length=300)
+    def __str__(self):
+        return self.feature
+
+class Project_Skill(models.Model):
+    project=models.ForeignKey(Project,on_delete=models.CASCADE,related_name='skill')
+    skill=models.CharField(max_length=50)
+    
+                                                                                                       
